b261edb167d1ea79abc3bf0667d6463c61578d66
Eric Rochester <erochest@virginia.edu>
2014-01-17 10:42:17 -0500

    Removed java config cruft.

--- custom/plugins/java.bash
+++ custom/plugins/java.bash
@@ -6,16 +6,7 @@ elif [ -d /usr/lib/jvm/jre-1.6.0 ]; then
 else
 	JAVA_HOME=
 fi
-CATALINA_HOME=/Library/Tomcat/Home
-SCALA_HOME=/Library/Scala/Home
-CLOJURE_EXT=$HOME/.clojure.d/ext
-CLOJURE_OPTS="-Xms32M -Xms128M -server"
-JENAROOT=$HOME/bin/Jena-2.6.4
-TDBROOT=$HOME/bin/TDB-0.8.10
-
-PATH=$SCALA_HOME/bin:$JENAROOT/bin:$TDBROOT/bin:$PATH
-
-export JAVA_HOME CATALINA_HOME SCALA_HOME CLOJURE_EXT CLOJURE_OPTS JENAROOT TDBROOT
+export JAVA_HOME
 
 find-in-jar() {
 	target=$1
