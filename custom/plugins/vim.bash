95f9451b72becc8b02b3079cc65f98c1e64ffaba
Eric Rochester <erochest@virginia.edu>
2014-10-27 12:48:56 -0400

    Improved svim wrapper.

--- custom/plugins/vim.bash
+++ custom/plugins/vim.bash
@@ -1,23 +1,18 @@
 
-if [ -d /usr/local/Cellar/macvim/7.4-70/MacVim.app/Contents/MacOS/ ]; then
-	PATH=$PATH:/usr/local/Cellar/macvim/7.4-70/MacVim.app/Contents/MacOS/
-fi
-
 function svim() {
-	if [ -f Session.vim ]
-	then
-		vim -S Session.vim "$@"
-	else
-		vim "$@"
-	fi
-}
+	ACCUM="vim"
 
-function pvim() {
-	if [ -f project.vim ]
-	then
-		vim -S project.vim "$@"
+	if [ -f Session.vim ]; then
+		ACCUM="$ACCUM -S Session.vim"
 	else
-		vim "$@"
+		ACCUM="$ACCUM +Obsession"
 	fi
+
+	if [ -f .project.vim ]; then
+		ACCUM="$ACCUM -S .project.vim"
+	fi
+
+	ACCUM="$ACCUM $@"
+	$ACCUM
 }
 
