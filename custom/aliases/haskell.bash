ba3d072d8a1e857b4afe7f25ba1c3767c71f3bb3
Eric Rochester <erochest@virginia.edu>
2014-11-10 15:02:30 -0500

    Removed haddock functions.

--- custom/aliases/haskell.bash
+++ custom/aliases/haskell.bash
@@ -12,14 +12,3 @@ alias yi='yesod init'
 alias yc='yesod configure'
 alias yd='yesod devel'
 
-function ls-haddock() {
-	pkg="$1"
-	find .cabal-sandbox -name index.html | grep "$pkg"
-}
-
-function open-haddock() {
-	pkg="$1"
-	haddock_dir=$(cabal sandbox hc-pkg field $pkg haddock-html | sed -e 's/haddock-html: //')
-	open "$haddock_dir/index.html"
-}
-
