a0d72ccf7f8e9255f17064f9960160850c6556f7
Eric Rochester <erochest@virginia.edu>
2015-07-20 11:04:55 -0400

    Added aliases for stack.

---
+++ custom/aliases/stack.bash
@@ -0,0 +1,10 @@
+
+alias st="stack"
+alias stb="stack build"
+alias sti="stack install"
+alias stt="stack test"
+alias stu="stack upload"
+alias stg="stack ghc"
+alias str="stack ghci"
+alias stc="stack clean"
+
